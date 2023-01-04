c8e8856706d54206230982554e98b94089c27cd8
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2023-01-05 01:28:28 +0530

    button and scrreen outer structure completed

---
+++ src/button.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+const Buttons = (props) => {
+    return (
+        <div className='buttons-container'>
+            <div className='center-circle'>
+                <h2>select</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Buttons;
---
+++ src/screen.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const Screens = (props) => {
+    return (
+        <div className='screen-container'>
+
+        </div>
+    );
+}
+
+export default Screens;
