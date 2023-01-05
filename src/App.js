f8503b98cf0d3745312ad383524a526e5cb62980
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2023-01-05 16:10:07 +0530

    uses zingchart to capture rotation around circle

--- src/App.js
+++ src/App.js
@@ -4,13 +4,56 @@ import './App.css';
 import Buttons from './button.js';
 import Screens from './screen.js';
 import React from 'react';
+import ZingTouch from 'zingtouch'
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      options: ['Games',
+                'Music',
+                'Settings',
+                'Cover Flow'
+    ],
+    change_in_angle : 0,
+    selected: 0
+    }
+  }
+  componentDidMount () {
+    //creatng region to listen for event
+    var buttonElement = ZingTouch.Region(document.getElementsByClassName('buttons-container')[0]);
+    buttonElement.bind(document.getElementsByClassName('buttons-container')[0], 'rotate', (event) => {
+      let dist = event.detail.distanceFromLast;
+      this.state.change_in_angle += dist;
+
+      if(this.state.change_in_angle > 60) {
+        console.log(this.state.options[this.state.selected]);
+                this.state.selected++;
+                this.state.change_in_angle = 0;
+                this.state.selected = this.state.selected % 4
+      }
+      else if(this.state.change_in_angle<-60) {
+        console.log(this.state.options[this.state.selected]);
+                this.state.selected--;
+                this.state.change_in_angle = 0;
+                if (this.state.selected === -1)
+                    this.state.selected = 3;
+                this.state.selected = this.state.selected % 4;
+      }
+    });
+
+    buttonElement.bind(document.getElementsByClassName('center-circle')[0], 'tap', (event) => {
+      event.stopPropogation();
+    });
+  }
   render() {
     return (
       <div className='App'>
         <Screens />
-        <Buttons />
+        <Buttons 
+        check={this.checker}
+        centerButton={this.centerButton}
+         />
       </div>
     );
   }
