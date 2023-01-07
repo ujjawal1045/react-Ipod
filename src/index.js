2126c528d6de9cda8768609f7508420c556d9dbc
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2023-01-07 17:59:06 +0530

    initialized firebse

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDopJjsS1Am72V-K5lEviwQIGiD__z31R0",
+  authDomain: "ujjawal-s-ipod.firebaseapp.com",
+  projectId: "ujjawal-s-ipod",
+  storageBucket: "ujjawal-s-ipod.appspot.com",
+  messagingSenderId: "229966145904",
+  appId: "1:229966145904:web:aeb3d34722ab75c98807b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
